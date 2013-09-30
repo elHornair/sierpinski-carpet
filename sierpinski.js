5f356a7ea73aba28b2c3ff772b2378428c4fc089
Alain Horner <alain.horner@liip.ch>
2013-09-30 14:46:44 +0200

    preparation for iterative drawing

--- sierpinski.js
+++ sierpinski.js
@@ -4,7 +4,7 @@ var sierpinski = (function () {
 
     var context,
 
-        drawCarpet = function (x, y, w, h) {
+        drawCarpet = function (x, y, w, h, iteration) {
             var i;
 
             if (w >= 3 && h >= 3) {
@@ -12,11 +12,13 @@ var sierpinski = (function () {
                 w = w / 3;
                 h = h / 3;
 
-                context.fillRect(x + w, y + h, w, h);
-
-                for (i = 0; i < 9; i++) {
-                    if (i !== 4) {
-                        drawCarpet(x + (i % 3) * w, y + Math.floor(i / 3) * h, w, h);
+                if (iteration === 1) {
+                    context.fillRect(x + w, y + h, w, h);
+                } else {
+                    for (i = 0; i < 9; i++) {
+                        if (i !== 4) {
+                            drawCarpet(x + (i % 3) * w, y + Math.floor(i / 3) * h, w, h, iteration - 1);
+                        }
                     }
                 }
             }
@@ -25,7 +27,9 @@ var sierpinski = (function () {
         init = function () {
             context = document.getElementById('myCanvas').getContext('2d');
             context.fillStyle = '#ccc';
-            drawCarpet(0, 0, 400, 400);
+            drawCarpet(0, 0, 400, 400, 1);
+            drawCarpet(0, 0, 400, 400, 2);
+            drawCarpet(0, 0, 400, 400, 3);
         };
 
     init();
